413839f3cac1875e3580ea0dc2d10817fbebc88d
gerakln <georgynefedev@gmail.com>
2020-09-11 15:09:48 +0300

    change factorial algorithm

--- backend/src/main/kotlin/mi/lab/tasks/FactorialTaskFactory.kt
+++ backend/src/main/kotlin/mi/lab/tasks/FactorialTaskFactory.kt
@@ -12,7 +12,7 @@ class FactorialTaskFactory(
             processRunner.computeInProcess(coroutineScope) { onComplete ->
                 ProcessDescription(
                         "mi.lab.FactorialMainKt",
-                        "256m",
+                        "1g",
                         onProcessStart = {
                             it.write("${input.a}\n")
                             it.write("${input.b}\n")
