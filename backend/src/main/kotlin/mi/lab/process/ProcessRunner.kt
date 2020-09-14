cb122310be9a0efa580e9214a69da4173cbef79e
Georgy Nefedev <gnefedev@yandex-team.ru>
2020-09-14 20:08:11 +0300

    add some staff from master

--- backend/src/main/kotlin/mi/lab/process/ProcessRunner.kt
+++ backend/src/main/kotlin/mi/lab/process/ProcessRunner.kt
@@ -7,17 +7,24 @@ import org.springframework.stereotype.Component
 import java.io.*
 import java.lang.Runnable
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 import javax.annotation.PostConstruct
 import javax.annotation.PreDestroy
 
 private val cpuCount = Runtime.getRuntime().availableProcessors()
+
+//trade of between latency and throughput
+private val parallelism = cpuCount
 
 @Component
 class ProcessRunner {
     private companion object : KLogging()
 
     private val executorService = Executors.newSingleThreadExecutor();
-    private val channel = Channel<Pair<ProcessDescription, CompletableDeferred<*>>>(capacity = cpuCount)
+    private val channel = Channel<suspend () -> Unit>()
+
+    //only for tests. May be used for stats
+    val processCounter = AtomicInteger(0)
 
     @PreDestroy
     fun shutdown() {
@@ -27,37 +34,40 @@ class ProcessRunner {
 
     @PostConstruct
     fun init() {
-        repeat(cpuCount) {
+        repeat(parallelism) {
             GlobalScope.launch {
-                for ((processDescription, result) in channel) {
-                    if (result.isCancelled) continue
-
-                    try {
-                        val process = runProcess(processDescription)
-
-                        result.invokeOnCompletion {
-                            if (process.isAlive) {
-                                logger.warn { "canceling process" }
-                                process.destroy()
-                            }
-                        }
-                        result.join()
-                    } catch (e: Exception) {
-                        result.completeExceptionally(e)
-                    }
+                for (block in channel) {
+                    block()
                 }
             }
         }
     }
 
-    fun <R> computeInProcessAsync(processDescriptionSupplier: (onComplete: (R) -> Unit) -> ProcessDescription): Deferred<R> {
+    fun <R> computeInProcessAsync(
+            processDescriptionSupplier: (onComplete: (R) -> Unit) -> ProcessDescription
+    ): Deferred<R> {
         val result = CompletableDeferred<R>()
         return GlobalScope.async {
+            channel.send {
+                if (result.isCancelled) return@send
 
-            val processDescription = processDescriptionSupplier { result.complete(it) }
+                try {
+                    val process = runProcess(processDescriptionSupplier { result.complete(it) })
+                    processCounter.incrementAndGet()
 
-            channel.send(processDescription to result)
-
+                    result.invokeOnCompletion {
+                        processCounter.decrementAndGet()
+                        //it == null when job is ended normally, process will end normally
+                        if (process.isAlive && it != null) {
+                            logger.warn { "canceling process $it" }
+                            process.destroy()
+                        }
+                    }
+                    result.join()
+                } catch (e: Exception) {
+                    result.completeExceptionally(e)
+                }
+            }
             result.await()
         }.also {
             it.invokeOnCompletion {
--- backend/src/test/kotlin/mi/lab/FactorialTest.kt
+++ backend/src/test/kotlin/mi/lab/FactorialTest.kt
@@ -1,28 +1,51 @@
 package mi.lab
 
-import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import mi.lab.process.ProcessRunner
 import mi.lab.tasks.FactorialTask
 import mi.lab.tasks.TaskFactory
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.comparesEqualTo
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import java.math.BigDecimal
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.coroutines.EmptyCoroutineContext
 
 @SpringBootTest
 class FactorialTest @Autowired constructor(
-        private val factorialTaskFactory: TaskFactory<FactorialTask.Params, BigDecimal>
+        private val factorialTaskFactory: TaskFactory<FactorialTask.Params, BigDecimal>,
+        private val processRunner: ProcessRunner
 ) {
     @Test
-    fun factorial() {
+    fun factorial() = runBlocking {
         val params = FactorialTask.Params(123L, 1231312.toBigDecimal())
 
-        val result: Deferred<BigDecimal> = factorialTaskFactory.computeAsync(params)
+        val result = factorialTaskFactory.computeAsync(params).await()
 
-        runBlocking {
-            assertThat(result.await(), comparesEqualTo(418528.toBigDecimal()));
+        assertThat(result, comparesEqualTo(418528.toBigDecimal()));
+    }
+
+    @Test
+    fun factorialCancelling() = runBlocking {
+        val params = FactorialTask.Params(32345L, 1231312123.toBigDecimal())
+
+        val ended = AtomicBoolean(false)
+
+        val result = CoroutineScope(EmptyCoroutineContext).async {
+            factorialTaskFactory.computeAsync(params).await()
+            ended.set(false)
         }
+
+        result.cancel()
+        delay(200)
+        assertFalse(ended.get())
+        assertEquals(0, processRunner.processCounter.get())
     }
 }
